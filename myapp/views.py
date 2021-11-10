8bccc5cad9590daee7c47f8c0decff6372e30505
Affanzuhaib <84571478+Affanzuhaib@users.noreply.github.com>
2021-11-10 16:15:10 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,27 +1,48 @@
-from django.http.response import HttpResponse
-from django.shortcuts import redirect, render, HttpResponse
-from django.contrib import messages
+from django.http.request import HttpHeaders
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib import messages
 
-# Create your views here.
-def index(request):
-    return render(request,'index.html')
+def home(request):
+    return render(request,'home.html')
 
-def login(request):
-    if request.method =='POST':
-        Username=request.POST['Username']
-        pass1=request.POST['pass1']
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        print(username,fname,lname)
 
-        user=authenticate(Username=Username,Password=pass1)
-        if   user is not None:
-            login(request,user)
-            fname=user.first_name
-            return render(request,'index.html',{'fname':fname})
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.is_superuser = False
+        myuser.is_staff = False
+        myuser.save()
+        messages.success(request, "Your Account has been created succesfully!!")
+    return render(request,'signup.html')
+    #return HttpResponse('This is signup page')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            messages.success(request, "Logged In Sucessfully!!")
+            return render(request,"signup.html",{"fname":fname})
         else:
-            messages.error(request,"Bad credentails!")
-            return redirect('')
-    return render(request, 'login.html')
-
-def logout(request):
-    pass
+            messages.error(request, "Bad Credentials!!")
+            return redirect('signin')
+    
+    return render(request, "signin.html")
+  #return render(request,'signin.html')
